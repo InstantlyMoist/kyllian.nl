03d037903738c96b0e976206fe52cefa445bc3b7
InstantlyMoist <kyllian007@gmail.com>
2020-10-21 13:58:48 +0200

    Added animations

--- script/projects.js
+++ script/projects.js
@@ -1,18 +1,21 @@
 init();
 
+var offset = 0;
+
 async function init() {
     const fileRead = await fetch("./projects.json");
     const json = await fileRead.json();
     let finalHTML = '';
     json.forEach(project => {
         finalHTML += getParsedHTML(project);
+        offset += 0.25;
     });
     document.getElementById("projects").innerHTML = finalHTML;
 }
 
 function getParsedHTML(project) {
     return `
-    <a href="${project.href}">
+    <a href="${project.href}" class="fadeInTop" style="animation-delay:${offset}s">
                 <div id="project">
                     <p id="title">${project.title}</p>
                     <p id="description">${project.description}</p>
